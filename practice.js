e8cf48b60256a2b368a3969842336d9fccf9cef0
khushi-yunicorn <khushi.jaiswal@yunicorn.in>
2025-11-27 14:59:40 +0530

    3.1.2 String

---
+++ practice.js
@@ -0,0 +1,23 @@
+let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let data = "Khushi";
+console.log(text.length);
+
+console.log(text.charAt(3));
+
+console.log(text.concat(" "+data));
+
+console.log(text.at(7));
+
+console.log(text.slice(4, 9));
+
+console.log(text.substring(0, 10));
+
+console.log(data.toUpperCase());
+
+console.log(text.toLowerCase());
+
+console.log(text.trim());
+
+console.log(text.replace("ABC", "XYZ"));
+
+console.log(text.split(""));
