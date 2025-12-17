c5d52720a5cc664687eef4249ecacf7097b56e4a
khushi-yunicorn <khushi.jaiswal@yunicorn.in>
2025-12-17 18:15:46 +0530

    Delete adonis-app directory

--- adonis-app/app/validators/update_user.ts
+++
@@ -1,8 +0,0 @@
-import vine from '@vinejs/vine'
-
-export const updateUserValidator = vine.compile(
-  vine.object({
-    email: vine.string().email().optional(),
-    password: vine.string().minLength(6).optional(),
-  })
-)
